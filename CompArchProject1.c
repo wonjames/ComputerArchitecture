8f0b0bf59e78504ba09b223e719d58cb2cf16b23
sashasmol <sashasmol@gwu.edu>
2018-09-17 22:29:52 -0400

    Complete File

--- CompArchProject1.c
+++ CompArchProject1.c
@@ -3,48 +3,7 @@
 #include <string.h>
 #include <math.h>
 
-/* 
-Group number: 2
-Team members: James Won, Karen Mani, Sasha Smolyansky
-*/
-
-/* PROJECT 1 */
-
-/* In this assignment you will examine a program consisting of a number of functions - labelled ques1.c to ques15.c -- each of which tests your knowledge of bitwise C operations. Your goal is to determine what each function does and if there
-is a simpler way to implement the function. There are two parts to the requirements.  */
-
-/* Assume 32 bit integers */
-
-/* PART A: You need to determine what each function is doing .
-You have to determine what each function is doing WITHOUT running the code. You must provide an explanation for your answer - you will get no credit if you correctly identify the function but provide  an incorrect or insufficient/incomplete explanation.
- Once you determine the function, you should run the code to check your answer.
-To run the code to test your function, you will need to insert appropriate C code to call the function and print out the result.
- Once you are done showing what each function does, you should then answer part b. */
-
-/* PART B: For each function, can you write a simpler equivalent function in C (using any C operators) that produces the same result.
- By simpler, we are interested in fewer operations in C but also more readable code. */
-
-
-/*
- As an example, consider the answers to function ques0 - which you worked out in class.
-
- HANDIN Instructions: You must write out your answers to Part A in a report to be submitted (as a tarfile or zipfile, along with your code) - in addition to the report, you can include comments in your code that may provide a secondary explanation (in case
- your report has errors). You MUST provide an explanation for what the function does and justify your answer. Simply providing an example is NOT an explanation - so don't expect any credit if
- your answers are simply a collection of examples or code comments. You are welcome to provide formal proofs where appropriate, but use your judgement on whether to pursue a formal proof or not. It is preferable to write your answers as comments in the code - but do not feel pressured to do so.
- For part B, write out your code in this file with the answer (a simpler code if it exists) after each of the assigned functions in a manner similar to the example shown in ques0.c (i.e., the answer to function ques7.c should be named ans7.c).
-
- IMPORTANT: The code you submit must compile correctly (using gcc). If the code you submit does not compile without errors, you will get a zero for Part B of this project.
-
- GRADING: Each question has a specific number of points (i.e., not all questions have the same level of difficulty). Your grade will be based
- on the correctness of your answer, expressing the answer as a logical function where possible, and the efficiency and readability of your rewritten code.
-
- Collaboration: You can discuss the project with your teammate. You CANNOT refer to online sources to get help; you can refer to the C syntax documentations. Your report must include names of both members of the team -- failure to do so would imply you are claiming you worked on your own, and may constitute a violation of academic integrity policies.*/
-
-/* EXAMPLE
- this function is similar to CallMeLast in the inclass exercises */
-/*  function ques0 returns 1 if x=y and returns 0 if x is not equal to y */
-/* the function ans0 does the same in two C statements */
-
+/* question 0*/
 int ques0(int x, int y)
 {
     int temp;
@@ -54,7 +13,7 @@ int ques0(int x, int y)
     return !(temp);
 }
 
-/* Answer to PART B: */
+/* question 0 answer */
 int ans0(int x, int y){
     int z=0;
     if (x==y)  z= 0;
@@ -64,7 +23,6 @@ int ans0(int x, int y){
 }
 
 /* QUESTIONS */
-
 /* question 1 */
 int ques1(int x) {
     int z;
@@ -84,14 +42,8 @@ int ques1(int x) {
 
 /* question 1 answer */
 int ans1(int x) {
-    if(x == 0) {
-        return 1;
-    }
-    else 
-    {
-        return 0;
-    }
-
+    if(x == 0) return 1;
+    else return 0;
 }
 
 /* question 2 */
@@ -139,7 +91,6 @@ int ques3(int x) {
 int ans3(int x) {
   // if x is positive, return 1
   if (x > 0) return 1;
-  
   // if x is negative or 0, return 0
   else return 0;
 }
@@ -169,28 +120,22 @@ int ques4(int n) {
     //divides y by 2^n-1
     return x & y;
 }
-int ans4(int n)
-{
+
+/* question 4 answer */
+int ans4(int n) {
     int y = -2147483648;
     int value = n-1;
     double power;
     power = pow(2,value);
-    if(n == 0)
-    {
-        return 0;
-    }
-
-    else
-    {
-        y = y / power;
-        return y;
+    if(n == 0) {
+      return 0;
+    } else {
+      y = y / power;
+      return y;
     }
 }
 
-
-
 /* question 5 */
-
 int ques5(int x) {
     //if x is odd then result is 1000...0000
     //if x is even then result is 0000....0000
@@ -202,24 +147,19 @@ int ques5(int x) {
 
     //will return either -1 or 0 depending on if its even or odd
     return result;
-
 }
 
-int ans5(int x)
-{
-    if(x % 2 == 0)
-    {
+/* question 5 answer */
+int ans5(int x) {
+    if(x % 2 == 0) {
         return 0;
-    }
-
-    if(x % 2 == 1)
-    {
+    } 
+    if (x % 2 == 1) {
         return -1;
     }
 }
 
 /* question 6 */
-
 int ques6(void) {
     int byte = 0x55;
     int word = byte | byte<<8;
@@ -227,21 +167,20 @@ int ques6(void) {
     return word | word<<16;
 }
 
-int ans6()
-{
-	return(1431655765);
+/* question 6 answer */
+int ans6() {
+	return 1431655765;
 }
-    /* question 7 */
 
+/* question 7 */
 int ques7(int x) {
-
-    return x & (~x+1);
+  return x & (~x+1);
 }
 
+/* question 7 answer */
 int ans7(int x) {
-	
-    return x & (~x+1); //takes x and 2's complement of x and returns the GCF of both decimal versions of the numbers
- }  
+	return x & (~x+1); //takes x and 2's complement of x and returns the GCF of both decimal versions of the numbers
+}  
 
 /* question 8 */
 int ques8(int x) {
@@ -260,6 +199,7 @@ int ques8(int x) {
     return y | z;
 }
 
+/* question 8 answer */
 int ans8(int x) {
   // if x is positive, return 1
   if (x > 0) return 1;
@@ -270,7 +210,6 @@ int ans8(int x) {
 }
 
 /* question 9 */
-
 int ques9(int x, int n, int c) {
     int n8 = n << 3; //calculates number of bits in a byte when multiplying by 8
     int mask = 0xff << n8;
@@ -280,70 +219,70 @@ int ques9(int x, int n, int c) {
     return (z  | cshift);
 }
 
+/* question 9 answer */
+int ans9(int x, int n, int c) {
+  int y = n<<3
+  int mask = 0xff << y;
+  int cshift = c << y;
+  int z = (x & ~mask);
+  return (z | cshift);
+}
 
 /* question 10 */
-
 int ques10(int x) {
 	int y = !!x;
 	int z = (!(x+x));
-
-    return y & z;
+  return y & z;
 }
 
+/* question 10 answer */
 int ans10(int x) {
-  if(x==0)
-  {
+  if(x==0) {
     return x;
-  }
-  else
-  {
+  } else {
     return(!x);
   }
 }
 
 /* question 11 */
-
 int ques11(int x, int y) {
 	int a = x >> 31;
 	int b = y >> 31;
-
-    return !((!a & b) | (!(a ^ b) & (y+~x)>>31));
+  return !((!a & b) | (!(a ^ b) & (y+~x)>>31));
 }
 
-int ans11(int x, int y)
-{
-  
-  if(x>0 || x==0) //when input x is either 0 or positive
-  {
-    if(y==0 || y<0) //if y’s input is either 0 or negative
-    {
-      return 0; //code will return a 0
-    }
-    else if(y>0) //but if y is positive
-    {
-      return 1; //code will return a 1
+/* question 11 answer */
+int ans11(int x, int y) {
+  //when input x is either 0 or positive
+  if(x>0 || x==0) {
+    //if y’s input is either 0 or negative
+    if(y==0 || y<0) {
+      //code will return a 0
+      return 0; 
+    //but if y is positive
+    } else if(y>0) {
+      //code will return a 1
+      return 1;
     }
   }
- if(x<0) //when input x is negative
-  {
-    if(y==0 || y>0) //if y’s input is 0 or positive
-    {
-      return 1; //code will return a 1
-    }
-    else if(y<0) //but if y is negative
-    {
-      if(x>y) //and x is greater than y
-      {
-        return 0; //code will return a 0
-      }
-      else
-      {
+  //when input x is negative
+  if(x<0) {
+    //if y’s input is 0 or positive
+    if(y==0 || y>0) {
+      //code will return a 1
+      return 1; 
+    //but if y is negative
+    } else if(y<0) {
+      //and x is greater than y
+      if(x>y) {
+        //code will return a 0
+        return 0; 
+      } else {
         return 1; //otherwise code returns a 1
       }
     }
   } 
 }
-
 
 /* question 12 */
 int ques12(int x, int m, int n) {
@@ -355,21 +294,19 @@ int ques12(int x, int m, int n) {
     return !((a|b) >> 31); //OR's a and b and shifts 31 positions, then inverts that value
 }
 
+/* question 12  answer*/
 int ans12(int x, int m, int n) {
    if(x>m) { //if m<x<n, code will return 0 otherwise it will return 1
      if(n>x) {
        return 0;
      }
+   } else {
+      return 1;
    }
-   else {
-       return 1;
-     }
-   }
+}
 
 /* question 13 */
-
 int ques13(int x) {
-
     int mask1, mask2, mask4, mask8, mask16;
 
     mask2 = 0x33 + (0x33 << 8);
@@ -389,11 +326,10 @@ int ques13(int x) {
     return x;
 }
 
-int ans13(int x)
-{
+/* question 13 answer */
+int ans13(int x) {
   int cursor = 0;
-  while (x)
-  {
+  while (x) {
     cursor += 1; //searches for if there is a 1 in the binary number and sums up number of them
     x >>= 1; //shifts to next bit to search again
   }
@@ -411,29 +347,22 @@ int ques14(int x) {
     return result;
 }
 
-int ans14(int x){
+/* question 14 answer */
+int ans14(int x) {
   int temp = 0; 
-  int count;
-  while(x)
-  {
-    count += x & 1;
+  while(x) {
+    temp += x & 1;
     x >> 1;
   }
-
-  if (count % 2 == 1) 
-  {
+  if (count % 2 == 1) {
 	  return 1;
-  }
-  else 
-  {
+  } else {
 	  return 0;
   }
   return 0;
-  
 }
 
 /* question 15 */
-
 int ques15(int x, int n) {
     /* for ques15 only, assume n is not a negative number  */
 
@@ -445,6 +374,7 @@ int ques15(int x, int n) {
     return (z & x);
 }
 
+/* question 15 answer */
 int ans15(int x, int n) {
   if (x < 0) {
 	  // if x < 0, absolute value x mod 2^n 
@@ -456,29 +386,110 @@ int ans15(int x, int n) {
 }
 
 
-int
-main(){
-    int a, b, c;
+int main() {
+  int a, b, c;
+  int t1;
 
-    int t1;
-
- 	printf("Enter first number, an integer stored into variable A preferably between 1 and 20:");
+ 	printf("Enter First Number (int 1 - 20):");
  	scanf ("%d",&a);
 	printf("\n");
- 	printf("Enter second number, an integer stored into variable B preferably between 1 and 20:");
+ 	printf("Enter Second Number (int 1 - 20):");
  	scanf ("%d", &b);
-    printf("Enter third number, an integer stored into variable C preferably between 1 and 20:");
+  printf("Enter Third Number (int 1 - 20):");
  	scanf ("%d", &c);
 	printf("\n");
+  
+	printf("You entered a= %d b= %d c=%d\n", a,b,c);
 
-
-	printf("you entered a= %d b= %d  c=%d\n", a,b,c);
-
-
-	t1=ques1(a);
-	printf("output of ques4 is t1 = %d  \n", t1);
-    /* To test/run the functions, you will need to input numbers and then call each of the functions, and print the return value */
-
+  // Question 1
+  t1 = ques1(a);
+  printf("Question 1: %d\n", t1);
+  t1 = ans1(a);
+  printf("Question 1 Answer: %d\n", t1);
+  
+  // Question 2
+  t1 = ques2(a);
+  printf("Question 2: %d\n", t1);
+  t1 = ans2(a);
+  printf("Question 2 Answer: %d\n", t1);
+  
+  // Question 3
+  t1 = ques3(a);
+  printf("Question 3: %d\n", t1);
+  t1 = ans3(a);
+  printf("Question 3 Answer: %d\n", t1);
+  
+  // Question 4
+  t1 = ques4(a);
+  printf("Question 4: %d\n", t1);
+  t1 = ans4(a);
+  printf("Question 4 Answer: %d\n", t1);
+  
+  // Question 5
+  t1 = ques5(a);
+  printf("Question 5: %d\n", t1);
+  t1 = ans5(a);
+  printf("Question 5 Answer: %d\n", t1);
+  
+  // Question 6
+  t1 = ques6();
+  printf("Question 6: %d\n", t1);
+  t1 = ans6();
+  printf("Question 6 Answer: %d\n", t1);
+  
+  // Question 7
+  t1 = ques7(a);
+  printf("Question 7: %d\n", t1);
+  t1 = ans7(a);
+  printf("Question 7 Answer: %d\n", t1);
+  
+  // Question 8
+  t1 = ques8(a);
+  printf("Question 8: %d\n", t1);
+  t1 = ans8(a);
+  printf("Question 8 Answer: %d\n", t1);
+  
+  // Question 9
+  t1 = ques9(a,b,c);
+  printf("Question 9: %d\n", t1);
+  t1 = ans9(a,b,c);
+  printf("Question 9 Answer: %d\n", t1);
+  
+  // Question 10
+  t1 = ques10(a);
+  printf("Question 10: %d\n", t1);
+  t1 = ans10(a);
+  printf("Question 10 Answer: %d\n", t1);
+  
+  // Question 11
+  t1 = ques11(a,b);
+  printf("Question 11: %d\n", t1);
+  t1 = ans11(a,b);
+  printf("Question 11 Answer: %d\n", t1);
+  
+  // Question 12
+  t1 = ques12(a,b,c);
+  printf("Question 12: %d\n", t1);
+  t1 = ans12(a,b,c);
+  printf("Question 12 Answer: %d\n", t1);
+  
+  // Question 13
+  t1 = ques13(a);
+  printf("Question 13: %d\n", t1);
+  t1 = ans13(a);
+  printf("Question 13 Answer: %d\n", t1);
+  
+  // Question 14
+  t1 = ques14(a);
+  printf("Question 14: %d\n", t1);
+  t1 = ans14(a);
+  printf("Question 14 Answer: %d\n", t1);
+  
+  // Question 15
+  t1 = ques15(a,b);
+  printf("Question 15: %d\n", t1);
+  t1 = ans15(a,b);
+  printf("Question 15 Answer: %d\n", t1);
 
 	return 0;
 }
